ab5c3e2d12417f85f2dc89045d64edbacaa7bfc8
chronosirius <prashastbhatia@gmail.com>
2023-01-03 15:21:55 -0800

    add database, catalog

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template as rt, send_from_directory
 import stripe
+from db import Database
 
 app = Flask(__name__)
+products = Database('/products/')
 
 @app.route('/')
 def index():
@@ -10,3 +12,10 @@ def index():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory('./', 'favicon.ico')
+
+@app.route('/catalog')
+def catalog():
+    return rt('catalog.html', top_products=products.values()[0:100])
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80)
