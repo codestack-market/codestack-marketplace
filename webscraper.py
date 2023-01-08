b7a80781aa4cda7b710eb5d67befa75a9b5db172
RedMythic1 <98300720+RedMythic1@users.noreply.github.com>
2023-01-08 16:16:54 +0530

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape(query):
+def scrape_py(query):
   # Make a request to the PyPI search page
   response = requests.get("https://pypi.org/search/?q=" + query)
 
@@ -9,7 +9,7 @@ def scrape(query):
   web = BeautifulSoup(response.content, "html.parser")
 
   # Find all the h3 elements that contain the package names
-  package_elements = web.find_all('span', class_='package-snippet__name'
+  package_elements = web.find_all(class_='package-snippet__name')
 
   # Extract the package names 
   packages = []
