cc92537b136994fec3dda048304bc35cf2745202
Elijah <46640763+ghwosty@users.noreply.github.com>
2023-01-27 20:35:12 +0000

    add fetch

--- static/scripts/email-auth.js
+++ static/scripts/email-auth.js
@@ -3,3 +3,20 @@ if (!document.URL.includes("?id=")) {
 } else {
     _(".authContainer").css("display", "block")
 }
+
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+  
+  postData('/confirmEmailAuth', { id: document.URL.split("?id=")[1] })
+    .then((data) => {
+      console.dir(data);
+    });
